9c5b284ebe7fe00230df53080624ac0e02f74923
kurtmatthewjunealborote-dot <kurtmatthewjunealborote@gmail.com>
2026-03-12 13:30:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def home():
 @app.route('/student')
 def get_student():
   return jsonify({
-    "name": "Your Name",
-    "grade": 10,
-    "section": "Zechariah"
+    "name": "Kurt Matthew June alborote",
+    "grade": 3,
+    "section": "BSIT"
   })
